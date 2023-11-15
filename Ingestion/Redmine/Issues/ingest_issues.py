0834f86b1bf5e37230b99a094c6eda93f69791f6
kml0001 <kmychamo@gmail.com>
2023-11-15 11:46:00 -0500

    change ingest issues to local

--- Ingestion/Redmine/Issues/ingest_issues.py
+++ Ingestion/Redmine/Issues/ingest_issues.py
@@ -3,10 +3,11 @@ from Functions import ingest_functions, export_functions
 raw_issues = ingest_functions.iterative_get_rest_api(
     "issues", "https://redmine.generalsoftwareinc.com/issues.json?status_id=closed,"
 )
-# export_functions.to_json(raw_issues, "raw_issues.json")
 
-export_functions.to_json_s3(
-    raw_issues,
-    "bucketfor008182637297",
-    "redmine/issues/raw_data/raw_issues.json",
-)
+export_functions.to_json(raw_issues, "raw_issues.json")
+
+# export_functions.to_json_s3(
+#     raw_issues,
+#     "bucketfor008182637297",
+#     "students/camilo/redmine/projects/raw_data/raw_projects.json",
+# )
