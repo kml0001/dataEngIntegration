9563e4af4c0863a416fc17186b58dec63a7d2007
kml0001 <kmychamo@gmail.com>
2023-11-15 11:46:54 -0500

    fix a bug on flatten projects

--- Ingestion/Redmine/Projects/flatten_projects.py
+++ Ingestion/Redmine/Projects/flatten_projects.py
@@ -5,14 +5,12 @@ def flatten_project(projects):
     flattened_data = []
     for project in projects:
         flattened_project = project.copy()
-        custom_fields = dict(project).get("custom_fields")
-        parent = dict(project).get("parent")
-        if parent is not None:
-            project.pop("parent")
+        custom_fields = flattened_project.get("custom_fields")
+        parent = flattened_project.get("parent")
 
         # procesamiento del custom_field
         if custom_fields:
-            project.pop("custom_fields")
+            flattened_project.pop("custom_fields")
             for field in custom_fields:
                 flattened_project["custom_fields_id"] = field["id"]
                 flattened_project["custom_fields_name"] = field["name"]
@@ -20,6 +18,7 @@ def flatten_project(projects):
 
         # procesamiento del parent
         if parent is not None:
+            flattened_project.pop("parent")
             flattened_project["parent_id"] = dict(parent).get("id")
             flattened_project["parent_name"] = dict(parent).get("name")
         else:
@@ -30,18 +29,19 @@ def flatten_project(projects):
     return flattened_data
 
 
-# raw_projects = import_functions.from_json("raw_projects.json")
+raw_projects = import_functions.from_json("raw_projects.json")
 
-raw_projects = import_functions.from_json_s3(
-    "bucketfor008182637297", "redmine/projects/raw_data/raw_projects.json"
-)
+# raw_projects = import_functions.from_json_s3(
+#     "bucketfor008182637297", "redmine/projects/raw_data/raw_projects.json"
+# )
 
 flattened_projects = flatten_project(raw_projects)
 
-# export_functions.to_json(flattened_projects, "flattened_projects.json")
+export_functions.to_json(flattened_projects, "flattened_projects.json")
+export_functions.to_csv(flattened_projects, "flattened_projects.csv")
 
-export_functions.to_json_s3(
-    flattened_projects,
-    "bucketfor008182637297",
-    "redmine/projects/raw_data/raw_projects.json",
-)
+# export_functions.to_json_s3(
+#     flattened_projects,
+#     "bucketfor008182637297",
+#     "redmine/projects/raw_data/raw_projects.json",
+# )
