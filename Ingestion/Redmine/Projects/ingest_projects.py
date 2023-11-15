179469bf289d96219dc8d33b2c3a296b54976a20
kml0001 <kmychamo@gmail.com>
2023-11-15 11:47:46 -0500

    fix a bug on ingest projects & ingest_users

--- Ingestion/Redmine/Projects/ingest_projects.py
+++ Ingestion/Redmine/Projects/ingest_projects.py
@@ -1,7 +1,7 @@
 from Functions import ingest_functions, export_functions
 
 raw_projects = ingest_functions.iterative_get_rest_api(
-    "projects", "https://redmine.generalsoftwareinc.com/projects.json"
+    "projects", "https://redmine.generalsoftwareinc.com/projects.json?"
 )
 
 # export_functions.to_json(raw_projects, "raw_projects.json")
