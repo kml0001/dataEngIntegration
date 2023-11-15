e6351d84b4c71a54d937b513c6b214ae196f56c6
kml0001 <kmychamo@gmail.com>
2023-11-15 11:45:21 -0500

    add iterative_get_rest_api_admin_net () function
    add skip security warning !!!

--- Functions/ingest_functions.py
+++ Functions/ingest_functions.py
@@ -1,4 +1,8 @@
 import requests as request
+from urllib3.exceptions import InsecureRequestWarning
+from urllib3 import disable_warnings
+
+disable_warnings(InsecureRequestWarning)
 
 USER_KEY_REDMINE_COM = "047f85e0b24fe4d7651e576fedd11ad410336e2d"
 ADMIN_KEY_REDMINE_NET = "8b6fda64b68909ba178c8f783703bed30396eaf5"
@@ -12,6 +16,34 @@ def iterative_get_rest_api(dicc_key, get_request):
     i = 100
     url = get_request + "offset=0&limit=100"
     response = request.get(url, headers=headers)
+    resp = response.json()
+    values = resp.copy()
+
+    limit = values.get("total_count")
+
+    # Recuperar todos los issues
+    while i <= limit:
+        url = get_request + "?offset=" + str(i) + "&limit=100"
+        response = request.get(url, headers=headers)
+        resp = response.json()
+        values[dicc_key].extend(resp.get(dicc_key))
+        i += 100
+        print(i)
+
+    values = dict(values.items()).get(dicc_key)
+
+    return values
+
+
+def iterative_get_rest_api_admin_net(dicc_key, get_request):
+    headers = {"X-Redmine-API-Key": ADMIN_KEY_REDMINE_NET}
+    # Realizo una petición para saber el numero total de issues , aprovecho para cargar los primeros 100
+    # creo la variable values y la tomo como base para almacenar el resto de issues
+
+    i = 100
+    url = get_request + "offset=0&limit=100"
+    disable_warnings(InsecureRequestWarning)
+    response = request.get(url, headers=headers, verify=False)
     resp = response.json()
     values = resp.copy()
 
