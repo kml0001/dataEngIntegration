9616c636399215a30d8b1ae7f3e989eee28454a1
kml0001 <kmychamo@gmail.com>
2023-11-16 18:41:11 -0500

    change ingest issues to .net

--- Ingestion/Redmine/Issues/ingest_issues.py
+++ Ingestion/Redmine/Issues/ingest_issues.py
@@ -1,7 +1,7 @@
 from Functions import ingest_functions, export_functions
 
-raw_issues = ingest_functions.iterative_get_rest_api(
-    "issues", "https://redmine.generalsoftwareinc.com/issues.json?status_id=closed,"
+raw_issues = ingest_functions.iterative_get_rest_api_admin_net(
+    "issues", "https://redmine.generalsoftwareinc.net/issues.json?status_id=closed,"
 )
 
 export_functions.to_json(raw_issues, "raw_issues.json")
