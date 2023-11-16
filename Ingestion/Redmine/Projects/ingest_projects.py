01c84fbc67ad3890b7780521a5d600d62eeb1118
kml0001 <kmychamo@gmail.com>
2023-11-16 18:40:02 -0500

    change ingest projects to .net

--- Ingestion/Redmine/Projects/ingest_projects.py
+++ Ingestion/Redmine/Projects/ingest_projects.py
@@ -1,13 +1,13 @@
 from Functions import ingest_functions, export_functions
 
-raw_projects = ingest_functions.iterative_get_rest_api(
-    "projects", "https://redmine.generalsoftwareinc.com/projects.json?"
+raw_projects = ingest_functions.iterative_get_rest_api_admin_net(
+    "projects", "https://redmine.generalsoftwareinc.net/projects.json?"
 )
 
-# export_functions.to_json(raw_projects, "raw_projects.json")
+export_functions.to_json(raw_projects, "raw_projects.json")
 
-export_functions.to_json_s3(
-    raw_projects,
-    "bucketfor008182637297",
-    "redmine/projects/raw_data/raw_projects.json",
-)
+# export_functions.to_json_s3(
+#     raw_projects,
+#     "bucketfor008182637297",
+#     "redmine/projects/raw_data/raw_projects.json",
+# )
